a7fd377b5eb4d3b3be22f44f24fadfc2e0b8d093
Himanshi Chainani <75713323+HimanshiChainani@users.noreply.github.com>
2023-06-13 20:37:08 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -49,7 +49,7 @@ const Projects = () => {
   return (
     <div
       name="projects"
-      className="bg-gradient-to-b from-black to-gray-900 w-full md:h-screen text-white"
+      className="bg-gradient-to-b from-black to-gray-800 w-full md:h-screen text-white"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-4">
